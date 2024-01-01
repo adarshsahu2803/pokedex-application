907d5f7dea0d8e7a1a521d0537e2875243e85e6c
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-02 00:51:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './router/Home';
 import './css/Home.css';
 import './css/taskBar.css';
@@ -7,11 +7,9 @@ import './css/taskBar.css';
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
