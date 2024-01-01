8f180b766858062e2e59fd4da5e39aa9287dab7e
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-02 01:19:57 +0530

    updated Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import SearchBar from './components/SearchBar.js';
 import TypeFilter from './components/TypeFilter.js';
 import React, { useState, useRef } from 'react';
 import TypeList from './components/TypeList.js';
-import Card from './components/Card.js';
 import { useNavigate } from 'react-router-dom';
 import './css/Home.css';
 import './css/taskBar.css';
