991da566c49b27976414666e1422d92ee26b5a10
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-01 19:24:51 +0530

    added card colors

--- src/components/card.js
+++ src/components/card.js
@@ -7,9 +7,10 @@ const Card = ({ pokemonId }) => {
   const [pokemonData, setPokemonData] = useState(null);
   const [isVisible, setIsVisible] = useState(false);
   const [pokemonID, setPokemonID] = useState(0);
+  const [color, setColor] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {      
+    const fetchData = async () => {
       try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
         setPokemonID(response.data.id)
@@ -18,9 +19,22 @@ const Card = ({ pokemonId }) => {
         console.error('Error fetching Pokemon data:', error);
       }
     };
-
     fetchData();
   }, [pokemonId]);
+
+  useEffect(() => {
+    const fetchColor = async () => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`);
+        console.log(response.data.color.name);
+        setColor(response.data.color.name);
+      } catch (error) {
+        console.error('Error fetching Pokemon color:', error);
+      }
+    }
+
+    fetchColor()
+  }, [pokemonId])
 
   const handleClick = () => {
     setIsVisible(!isVisible);
@@ -35,9 +49,22 @@ const Card = ({ pokemonId }) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
+  const colorMap = new Map([
+    ['red', 'rgb(255, 90, 90)'], 
+    ['green', 'rgb(160, 160, 160)'], 
+    ['blue', 'rgb(125, 125, 210)'], 
+    ['black', 'rgb(101, 101, 101)'], 
+    ['purple', 'rgb(187, 79, 187)'], 
+    ['brown', 'rgb(157, 100, 100)'], 
+    ['white', 'rgb(169, 156, 23)'], 
+    ['yellow', 'rgb(164, 164, 0)'], 
+    ['pink', 'rgb(208, 136, 145)'], 
+    ['gray', 'rgb(211, 193, 196)'], 
+  ]);
+
   return (
     <div onClick={handleClick}>
-      <div className="pokemon-card">
+      <div className="pokemon-card" style={{ backgroundColor: colorMap.get(color) }}>
         {pokemonData && (
           <>
             <div className="pokemon-details">
